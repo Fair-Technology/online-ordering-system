9056c80897b759924dfe0000097e3676e68a14de
Manish Ghimire <manishghimire@hotmail.com>
2025-10-23 11:37:35 +1100

    Account creation loading spinner page

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { superAdminApi } from "./api/superAdminApi";
-import { itemApi } from "./api/itemApi";
+import { configureStore } from '@reduxjs/toolkit';
+import { itemApi } from './api/itemApi';
 
 export const store = configureStore({
   reducer: {
-    [superAdminApi.reducerPath]: superAdminApi.reducer,
     [itemApi.reducerPath]: itemApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(superAdminApi.middleware, itemApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
